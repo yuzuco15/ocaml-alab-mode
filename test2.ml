2d3c906c9b375b38d6012d67515e0f12e51f7a51
Yuki Ishii <yuzu.or.orange@gmail.com>
2015-12-10 15:53:00 +0900

    add *.cmt to .gitignore

--- test2.ml
+++ test2.ml
@@ -1,6 +1,6 @@
 type tree = Empty | Node of tree * int * tree
 
-let rec count (r: string): ((int * int * int) * (int * int) * string) = ((exit(*{r }*)0, exit(*{ }*)1, exit(*{ }*)2)
+let rec count (r: string): ((int * int * int) * (int * int) * string) = ((exit(*{ }*)0, exit(*{ }*)1, exit(*{ }*)2)
 								      , (0, 1
 									)
 								      , r
