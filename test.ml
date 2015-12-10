fd484d89083fe186646377c2932bc05aef665b9d
Yuki Ishii <yuzu.or.orange@gmail.com>
2015-12-10 23:26:13 +0900

    support refining record

--- test.ml
+++ test.ml
@@ -1,7 +1,11 @@
 type t = { a : int; b : float; c : string }
 
 let f x = x + 1;;
-let g {a = a; b = b; c = c} (t: t) = match t with
-{a = a, b = b, c = c} -> exit(*{ }*)0
+let g (r: t): t = {a = exit(*{ }*), b = exit(*{ }*), c = exit(*{ }*)}
+
+
+
+
+
 
 
--- test2.ml
+++ test2.ml
@@ -1,11 +1,6 @@
 type tree = Empty | Node of tree * int * tree
 
-let rec count (r: string): ((int * int * int) * (int * int) * string) = ((exit(*{ }*)0, exit(*{ }*)1, exit(*{ }*)2)
-								      , (0, 1
-									)
-								      , r
-								      )
-
-
+let rec count (r: string): ((int * int * int) * (int * int) * string) = ((exit(*{ }*), exit(*{ }*), exit(*{ }*))
+, exit(*{ }*)0, exit(*{ }*)0)
 
 
